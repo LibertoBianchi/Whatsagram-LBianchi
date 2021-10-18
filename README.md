7fc00053820eea70db2841d1185ee5b4ccb7dd20
Liberto <lbianchi@cifpfbmoll.eu>
2021-10-18 19:38:56 +0200

    feat: whatsagram, Funcionalidad Teclas

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Whatsagram-LBianchi
+## | TODO list |
+- [x] Funcionalidad Teclas
+- [ ] Enviar texto a textArea
+- [ ] Crear Interfaz
+- [ ] Emojis
---
+++ src/js/whatsappgram.js
@@ -0,0 +1,51 @@
+var mayus = 1;
+
+function enviar() {
+    console.log(document.getElementById("mensaje").value);
+    document.getElementById("mensaje").value = "";
+    mayus = 1
+}
+
+function mandarLetra(letra) {
+    if (mayus == 0) {
+        document.getElementById("mensaje").value = document.getElementById("mensaje").value + letra;
+    } else if (mayus == 1) {
+        document.getElementById("mensaje").value = document.getElementById("mensaje").value + letra.toUpperCase();
+        mayus = 0
+    } else if (mayus == 2) {
+        document.getElementById("mensaje").value = document.getElementById("mensaje").value + letra.toUpperCase();
+    }
+}
+
+function saltoLinea() {
+    document.getElementById("mensaje").value = document.getElementById("mensaje").value + "\n";
+}
+
+function eliminarCaracter(a) {
+    var texto = document.getElementById("mensaje").value.trim();
+    if (a == "delante") {
+        document.getElementById("mensaje").value = texto.slice(0, -1);
+    } else if (a == "atras") {
+        document.getElementById("mensaje").value = texto.slice(1)
+    };
+}
+
+function limpiarTexto() {
+    document.getElementById("mensaje").value = "";
+}
+
+function eliminarUltimaPalabra() {
+    var texto = document.getElementById("mensaje").value.trim();
+    var ultimoEspacio = texto.lastIndexOf(" ");
+    document.getElementById("mensaje").value = texto.substring(0, ultimoEspacio);
+}
+
+function mayuscula() {
+    if (mayus == 0) {
+        mayus = 1
+    } else if (mayus == 1) {
+        mayus = 2
+    } else if (mayus == 2) {
+        mayus = 0
+    }
+}
