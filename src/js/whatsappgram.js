d50fcdc179a0b8e4a2f724df784ce06fa0d74fb3
LibertoBianchi <lbianchi@cifpfbmoll.eu>
2021-10-29 20:59:28 +0200

    Feat: Css (no teminado) y funcionalidad

--- src/js/whatsappgram.js
+++ src/js/whatsappgram.js
@@ -1,24 +1,56 @@
 var mayus = 1;
-
+var emoji = 1
+var fechaControl = 0;
 
 function enviar() {
+    console.log(document.getElementById("mensaje").value)
+    var fecha = new Date();
+    var horaMsj = fecha.getHours() + ":" + fecha.getMinutes();
+    var horaTxt = horaMsj.toLocaleString();
+
+    var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+    var diaMes = new Date();
+    diaMes = "<p class="+"fechas"+">"+(diaMes.getDate()+" de "+meses[diaMes.getMonth()]).toLocaleString()+"</p>";
+
+if(fechaControl==0){
+    
+    document.getElementById("messagingArea").innerHTML = diaMes
+    fecha++
+}
+
     if (document.getElementById("mensaje").value.trim() != "") {
         var mensaje = document.createElement("p");
-        var enviado = document.createTextNode(document.getElementById("mensaje").value);
-        mensaje.appendChild(enviado);
-        document.getElementById("messagingArea").appendChild(mensaje);
+        var horaEnv = document.createElement("span")
+        var enviado = document.createTextNode(document.getElementById("mensaje").value + " " + horaTxt);
+        var horaNodo = document.createTextNode(horaTxt)
+
+        
+
+        horaEnv.append(horaNodo);
+        mensaje.append(enviado);
+
+        
+
+        document.getElementById("messagingArea").append(mensaje);
+
         document.getElementById("mensaje").value = "";
         mayus = 1
+
     }
 }
 
 function mandarLetra(letra) {
     if (mayus == 0) {
         document.getElementById("mensaje").value = document.getElementById("mensaje").value + letra;
-    } else if (mayus == 1) {
+    }
+
+    else if (mayus == 1) {
         document.getElementById("mensaje").value = document.getElementById("mensaje").value + letra.toUpperCase();
         mayus = 0
-    } else if (mayus == 2) {
+    }
+
+    else if (mayus == 2) {
         document.getElementById("mensaje").value = document.getElementById("mensaje").value + letra.toUpperCase();
     }
 }
@@ -29,9 +61,11 @@ function saltoLinea() {
 
 function eliminarCaracter(a) {
     var texto = document.getElementById("mensaje").value.trim();
+
     if (a == "delante") {
         document.getElementById("mensaje").value = texto.slice(0, -1);
-    } else if (a == "atras") {
+    }
+    else if (a == "atras") {
         document.getElementById("mensaje").value = texto.slice(1)
     };
 }
@@ -44,13 +78,15 @@ function limpiarTexto() {
 function eliminarUltimaPalabra() {
     var texto = document.getElementById("mensaje").value.trim();
     var ultimoEspacio = texto.lastIndexOf(" ");
+
     document.getElementById("mensaje").value = texto.substring(0, ultimoEspacio);
 }
-//⇪ ⇧
+
 function mayuscula() {
     if (mayus == 0) {
         mayus = 1
         document.getElementById("mayus").value = "⇪"
+
     } else if (mayus == 1) {
         mayus = 2
         document.getElementById("mayus").classList.add('mayusPerma');
@@ -60,7 +96,19 @@ function mayuscula() {
         mayus = 0
         document.getElementById("mayus").classList.remove('mayusPerma');
         document.getElementById("mayus").value = "⇧"
-    
-        document.getElementsByTagName('input[type=button]').style.textTransform = "lowercase"
     }
+}
+
+function verEmoji(){
+    console.log("funciona")
+if (emoji==1){
+    document.getElementById("emojiArea").classList.remove('emojiAreaInv');
+    document.getElementById("emojiArea").classList.add('emojiAreaVis');
+    emoji = 0;
+}
+else if(emoji==0){
+    document.getElementById("emojiArea").classList.remove('emojiAreaVis');
+    document.getElementById("emojiArea").classList.add('emojiAreaInv');
+    emoji = 1;
+}
 }
