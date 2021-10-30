71e202594f0c193ed092b17cf237410faecb267c
LibertoBianchi <lbianchi@cifpfbmoll.eu>
2021-10-30 17:47:35 +0200

    Feat: Proyecto terminado

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Whatsagram-LBianchi
+## ________________________________________________________
+## | Font |
+## font : 'Poppins', sans-serif
+## ________________________________________________________
 ## | TODO list |
 - [x] Funcionalidad Teclas
 - [x] Enviar texto a missageArea
-- [ ] Crear Interfaz
-- [ ] Emojis
+- [x] Crear Interfaz
+- [x] Emojis
+## _______________________________________________________
+## | Extras |
+## -Emojis Insertados en mensajes
+## -Tras poner un punto la siguiente letra es mayuscula
+## -Es responsive en cualquier dispositivo
+## _______________________________________________________
--- src/js/whatsappgram.js
+++ src/js/whatsappgram.js
@@ -1,63 +1,74 @@
+//Variables globales para control
 var mayus = 1;
 var emoji = 1
 var fechaControl = 0;
 
+//Enviará los mensajes del mensaje a la missagingArea
 function enviar() {
-    console.log(document.getElementById("mensaje").value)
+    //Creamos la hora para unirla al mensaje posteriormente
     var fecha = new Date();
-    var horaMsj = fecha.getHours() + ":" + fecha.getMinutes();
-    var horaTxt = horaMsj.toLocaleString();
+    var horaMsjHoras = fecha.getHours() + ":"
+    var horaMsjMin = +fecha.getMinutes();
 
-    if (fecha.getMinutes < 10) {
-        horaMsj = fecha.getHours() + ":" + "0" + fecha.getMinutes()
+    var horaMinTxt = horaMsjMin.toLocaleString();
 
-    }
-
+    //array para formatear el dia
     var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+        //si los minutos son menores que 10 añade un cero a los minutos
+    console.log(horaMinTxt)
+    if (horaMinTxt < 10) {
+        horaTxt = fecha.getHours() + ":" + "0" + fecha.getMinutes()
 
+    } else {
+        horaTxt = fecha.getHours() + ":" + fecha.getMinutes()
+    }
+    console.log(horaMinTxt)
+        //variable que printearemos como el dia en que se envia el mensaje
     var diaMes = new Date();
     diaMes = "<h1 class=" + "fechas" + ">" + (diaMes.getDate() + " de " + meses[diaMes.getMonth()]).toLocaleString() + "</h1>";
 
+    //Control para que el dia solo se printe una vez
     if (fechaControl == 0) {
-
         document.getElementById("messagingArea").innerHTML += diaMes
         fechaControl++
     }
 
+    //Si el mensaje no esta vacío crea y envia al messagingArea el mensaje y la hora
     if (document.getElementById("mensaje").value.trim() != "") {
-        var mensaje = document.createElement("p");
-        var horaEnv = document.createElement("span");
 
-
-
-        document.getElementById("messagingArea").innerHTML += "<p>" + document.getElementById("mensaje").value + " " + "<font style='font-size:2vh'>" + horaTxt + "</font>" + "</p>";
-
-
-
+        document.getElementById("messagingArea").innerHTML += "<p>" + document.getElementById("mensaje").value + " " + "<font style='font-size:1.5vh'>" + horaTxt + "</font>" + "</p>";
+        //vaciamos el valor del mensaje y ponermos las mayusculas en true
         document.getElementById("mensaje").value = "";
         mayus = 1
-
+        document.getElementById("mayus").value = "⇪"
     }
 }
 
+//Recibe de parametro una letra enviada y la envia al mensaje
 function mandarLetra(letra) {
+    //Comprobará la variable mayuscula y dependiendo de su valor pondrá mayusculas o cambiará su estado
     if (mayus == 0) {
         document.getElementById("mensaje").value = document.getElementById("mensaje").value + letra;
+        //En caso de que el valor letra sea un punto, la próxima letra será escrita en mayuscula
         if (letra == ".") {
             mayus = 1
         }
     } else if (mayus == 1) {
         document.getElementById("mensaje").value = document.getElementById("mensaje").value + letra.toUpperCase();
-        mayus = 0
+        mayus = 0;
+        document.getElementById("mayus").value = "⇧"
+
     } else if (mayus == 2) {
         document.getElementById("mensaje").value = document.getElementById("mensaje").value + letra.toUpperCase();
     }
 }
 
+//Se añadirá un br al valor de mensaje
 function saltoLinea() {
-    document.getElementById("mensaje").value = document.getElementById("mensaje").value + "<br>";
+    document.getElementById("mensaje").value = document.getElementById("mensaje").value + " <br> ";
 }
 
+//Recibe un parametro, segun su valor elimina alante o elimina atrás
 function eliminarCaracter(a) {
     var texto = document.getElementById("mensaje").value.trim();
 
@@ -68,11 +79,13 @@ function eliminarCaracter(a) {
     };
 }
 
+//Da un valor vacio al string y la proxima letra será mayuscula
 function limpiarTexto() {
     document.getElementById("mensaje").value = "";
     mayus = 1;
 }
 
+//Busca el último espacio y elimina la palabra antes que ese ultimo espacio
 function eliminarUltimaPalabra() {
     var texto = document.getElementById("mensaje").value.trim();
     var ultimoEspacio = texto.lastIndexOf(" ");
@@ -80,6 +93,7 @@ function eliminarUltimaPalabra() {
     document.getElementById("mensaje").value = texto.substring(0, ultimoEspacio);
 }
 
+//Cambia el valor de la global mayus y cambia el estilo de la tecla mayusculas
 function mayuscula() {
     if (mayus == 0) {
         mayus = 1
@@ -97,12 +111,13 @@ function mayuscula() {
     }
 }
 
+//Cambia el valor de la globl emoji para hacer aparecer y desaparecer el emojiArea
 function verEmoji() {
     console.log("funciona")
     if (emoji == 1) {
         document.getElementById("emojiArea").classList.remove('emojiAreaInv');
         document.getElementById("emojiArea").classList.add('emojiAreaVis');
-        document.getElementById("emojiButton").value = "Txt"
+        document.getElementById("emojiButton").value = "🄰"
         emoji = 0;
     } else if (emoji == 0) {
         document.getElementById("emojiArea").classList.remove('emojiAreaVis');
